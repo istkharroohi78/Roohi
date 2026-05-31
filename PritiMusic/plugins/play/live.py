07e0ad83a326c2064848dd19aa044c941d8bebf8
THE SHIV <shivofficial004@gmail.com>
2026-05-31 15:01:23 +0530

    Update live.py

--- PritiMusic/plugins/play/live.py
+++ PritiMusic/plugins/play/live.py
@@ -13,30 +13,38 @@ async def play_live_stream(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     callback_request = callback_data.split(None, 1)[1]
     vidid, user_id, mode, cplay, fplay = callback_request.split("|")
+    
     if CallbackQuery.from_user.id != int(user_id):
         try:
             return await CallbackQuery.answer(_["playcb_1"], show_alert=True)
         except:
             return
+            
     try:
         chat_id, channel = await get_channeplayCB(_, cplay, CallbackQuery)
     except:
         return
+        
     video = True if mode == "v" else None
     user_name = CallbackQuery.from_user.first_name
     await CallbackQuery.message.delete()
+    
     try:
         await CallbackQuery.answer()
     except:
         pass
+        
     mystic = await CallbackQuery.message.reply_text(
         _["play_2"].format(channel) if channel else _["play_1"]
     )
+    
     try:
         details, track_id = await YouTube.track(vidid, True)
     except:
         return await mystic.edit_text(_["play_3"])
+        
     ffplay = True if fplay == "f" else None
+    
     if not details["duration_min"]:
         try:
             await stream(
@@ -57,4 +65,5 @@ async def play_live_stream(client, CallbackQuery, _):
             return await mystic.edit_text(err)
     else:
         return await mystic.edit_text("» ɴᴏᴛ ᴀ ʟɪᴠᴇ sᴛʀᴇᴀᴍ.")
+        
     await mystic.delete()
