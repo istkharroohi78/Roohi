6679da050b62668b668e5b593c10f5b06eee0c46
istkharroohi78 <istkharroohi@gmail.com>
2026-07-11 22:08:11 +0530

    Update inline.py

--- PritiMusic/cplugin/utils/inline.py
+++ PritiMusic/cplugin/utils/inline.py
@@ -1,5 +1,5 @@
 from pyrogram.types import InlineKeyboardMarkup
-from button import styled_button, ButtonStyle # Zaroori import
+from button import styled_button, ButtonStyle
 
 # --- OPTION 1: Static ---
 buttons = InlineKeyboardMarkup(
@@ -11,14 +11,13 @@ buttons = InlineKeyboardMarkup(
             styled_button(text="▢", callback_data="end_cb", style=ButtonStyle.DANGER),
         ],
         [
-            styled_button(text="『 ✦ 𝐂ʟᴏηє 𝐌є ✦ 』", url="https://t.me/SizzuMusicBot", style=ButtonStyle.SUCCESS)
+            styled_button(text="ᴀᴅᴅ ᴍᴇ", url="https://t.me/SizzuMusicBot?startgroup=true", style=ButtonStyle.SUCCESS)
         ],
     ]
 )
 
 close_key = InlineKeyboardMarkup(
     [
-        # "Add me" hata diya gaya hai, ab sirf Close bacha hai
         [
             styled_button(text="✯ CLOSE ✯", callback_data="close", style=ButtonStyle.DANGER)
         ]
@@ -29,21 +28,18 @@ close_key = InlineKeyboardMarkup(
 def stream_markup(chat_id):
     return InlineKeyboardMarkup(
         [
-            # Top Row: Basic Controls
             [
                 styled_button(text="▷", callback_data=f"ADMIN Resume|{chat_id}", style=ButtonStyle.SUCCESS),
                 styled_button(text="II", callback_data=f"ADMIN Pause|{chat_id}", style=ButtonStyle.DANGER),
                 styled_button(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}", style=ButtonStyle.PRIMARY),
                 styled_button(text="▢", callback_data=f"ADMIN Stop|{chat_id}", style=ButtonStyle.DANGER),
             ],
-            # Middle Row: Seek Buttons
             [
                 styled_button(text="<- 20s", callback_data=f"ADMIN SeekBack|{chat_id}", style=ButtonStyle.PRIMARY),
                 styled_button(text="20s + ->", callback_data=f"ADMIN SeekForward|{chat_id}", style=ButtonStyle.PRIMARY),
             ],
-            # Bottom Row: Clone & Close merged (Space bachane ke liye!)
             [
-                styled_button(text="『 ✦ 𝐂ʟᴏηє 𝐌є ✦ 』", url="https://t.me/SizzuMusicBot", style=ButtonStyle.SUCCESS),
+                styled_button(text="ᴀᴅᴅ ᴍᴇ", url="https://t.me/SizzuMusicBot?startgroup=true", style=ButtonStyle.SUCCESS),
                 styled_button(text="✯ CLOSE ✯", callback_data="close", style=ButtonStyle.DANGER)
             ]
         ]
