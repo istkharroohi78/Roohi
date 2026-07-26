ad93c4abd9669d047f8717c9bd1af373306bc814
istkharroohi78 <istkharroohi@gmail.com>
2026-07-27 01:10:52 +0530

    Update Youtube.py

--- PritiMusic/platforms/Youtube.py
+++ PritiMusic/platforms/Youtube.py
@@ -16,7 +16,7 @@ DOWNLOAD_DIR = "downloads"
 LOGGER = logging.getLogger(__name__)
 
 API_URL = os.environ.get("SHRUTI_API_URL", "https://api.shrutibots.site")
-API_KEY = os.environ.get("SHRUTI_API_KEY", "ShrutiBotsC0WH1GowF2HkGoKv4F3y")
+API_KEY = os.environ.get("SHRUTI_API_KEY", "ShrutiBotsDLP6onoUZGffodx4G6qY")
 
 # Worker API (Now Primary)
 WORKER_FALLBACK_API_URL = os.getenv("WORKER_FALLBACK_API_URL", "https://youtubenewapi.skybotsdeveloper.workers.dev")
